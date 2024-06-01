5ff1562f3a1f0c6a7ca993fbec902fdf624b29d8
Alex Didenko <alexdidenko7@gmail.com>
2024-06-01 17:08:14 +0200

    Initial commit

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,31 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Меню</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Панель dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Типы ТС</a>
+<ul>
+<li><a href="create-brand.php"> Создать тип ТС </a></li>
+<li><a href="manage-brands.php"> Управлять типами ТС </a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> ТС</a>
+					<ul>
+						<li><a href="post-avehical.php"> Опубликовать ТС</a></li>
+						<li><a href="manage-vehicles.php"> Управлять ТС</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Управлять бронями</a></li>
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Управлять отзывами</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop""></i> Управлять запросами</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Пользователи</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Управлять страницами</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Обновить контакты</a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Управлять подписчиками</a></li>
+
+			</ul>
+		</nav>
---
+++ includes/sidebar.php
@@ -0,0 +1,11 @@
+<div class="profile_nav">
+          <ul>
+            <li><a href="profile.php">Настройки профиля</a></li>
+              <li><a href="update-password.php">Обновление пароля</a></li>
+            <li><a href="my-booking.php">Мои бронирования</a></li>
+            <li><a href="post-testimonial.php">Опубликовать отзыв</a></li>
+               <li><a href="my-testimonials.php">Мои отзывы</a></li>
+            <li><a href="logout.php">Выход</a></li>
+          </ul>
+        </div>
+      </div>
